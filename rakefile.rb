f245936c2af12f0820b7106d4ce1e96ebb8f95fe
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2012-11-05 09:03:50 -0500

    albacore is awesome, builds nuget packages and puts them in a build folder. Addresses issue #1

--- rakefile.rb
+++ rakefile.rb
@@ -1,14 +1,24 @@
 require 'albacore'
 
-task :default => [:version, :build, :test]
+task :default => [:init, :version, :build, :test, :core, :web]
+
+desc "initalize" 
+task :init do |init| 
+	if !FileTest::directory?("./build")
+		Dir.mkdir("./build")
+	else
+		puts 'build directory already exists'
+	end
+
+	@version = file = File.read("version.txt")
+
+end
 
 desc "Version MvcFlash"
-assemblyinfo :version do |asm|
+assemblyinfo :version => :init do |asm|
 
-	file = File.read("version.txt")
- 
-  	asm.version = file
-  	asm.file_version = file
+  	asm.version = @version
+  	asm.file_version = @version
 
   	asm.company_name = "MvcFlash"
   	asm.product_name = "MvcFlash"
@@ -31,4 +41,38 @@ desc "Run unit tests"
 xunit :test => :build do |xunit|
 	xunit.command = "./packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
 	xunit.assembly = "./MvcFlash.Tests/bin/Release/MvcFlash.Tests.dll"
+	xunit.html_output = "./build/"
+end
+
+desc "Create the nuget packages for: MvcFlash.Core"
+nugetpack :core => :test do |nuget|
+	update_version("mvcflash.core.nuspec")
+
+	nuget.command = "./.nuget/nuget.exe"
+	nuget.nuspec = "mvcflash.core.nuspec"
+	nuget.output = "build/"
+end
+
+desc "Create the nuget packages for: MvcFlash.Web"
+nugetpack :web => :core do |nuget|
+	update_version("mvcflash.web.nuspec")
+
+	nuget.command = "./.nuget/nuget.exe"
+	nuget.nuspec = "mvcflash.web.nuspec"
+	nuget.output = "build/"
+end
+
+
+def update_version(file_path)
+	# modify the version number in the nuspec
+	initial = File.read(file_path)
+	doc = REXML::Document.new(initial)
+	doc.context[:attribute_quote] = :quote
+
+    doc.elements.each("//version") do |p|
+        p.text = @version
+    end
+
+	# save the new versioned nuspec
+	File.open(file_path , 'w') {|f| doc.write(f) }
 end
